4e350b5f5dff724a9e475cba8a914d00b6532e98
manu04292 <manugasparini04@gmail.com>
2026-05-06 17:48:10 +0200

    integrazione connessione mongoDB

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,40 @@
 import express from "express";
+import { MongoClient, ServerApiVersion } from 'mongodb';
 const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT;
+const uri=`mongodb+srv://career-service:${process.env.MONGO_PASSWORD}@clustercareerservice.fqehjf0.mongodb.net/?appName=ClusterCareerService`
+
+
+//creazione client per mongoDB
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+//connessione di prova mongoDB
+export async function runStableAPIConnect() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const result = await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+    return result;
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+
 
 // Elenco di origins permesse per chiamare  API
 // TODO: in futuro da inserire nell'array url effettivo
@@ -15,6 +47,7 @@ app.use(cors({
 
 
 app.get("/api/test", (req, res) => {
+    runStableAPIConnect().catch(console.dir);
     res.send("Hello world via GET!");
     console.log("Response sent get");
 });

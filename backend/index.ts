6acbd69d53cfc34ba46a9922395869497b3d2aba
Matteo Perazzoli <matteo.perazzoli04@gmail.com>
2026-05-07 20:52:42 +0200

    Fix bug CORS con better auth

--- backend/index.ts
+++ backend/index.ts
@@ -6,12 +6,6 @@ import { auth } from "./auth/auth";
 const app = express();
 const port = process.env.PORT;
 
-// Per gestione routes autenticazione. -> splat altrimenti non va (express 5)
-app.all("/api/auth/*splat", toNodeHandler(auth));
-
-// Middleware express. Va sotto altrimenti intercetta anche quelle di better-auth prima
-app.use(express.json());
-
 // Elenco di origins permesse per chiamare  API
 // TODO: in futuro da inserire nell'array url effettivo
 const allowedOrigins = ["http://localhost:5173"];
@@ -21,6 +15,12 @@ app.use(cors({
   origin: allowedOrigins,
   credentials: true,
 }));
+
+// Per gestione routes autenticazione. -> splat altrimenti non va (express 5)
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+// Middleware express. Va sotto altrimenti intercetta anche quelle di better-auth prima
+app.use(express.json());
 
 // Test chiamata per ottenere sessione
 app.get("/api/me", async (req, res) => {

cc566e0070fa938db640d1f98a3c039d02b92eba
Matteo Perazzoli <matteo.perazzoli04@gmail.com>
2026-08-24 22:42:34 +0200

    fix bug logico di aggiornamento

--- frontend/src/pages/admin/formEditor.tsx
+++ frontend/src/pages/admin/formEditor.tsx
@@ -464,7 +464,7 @@ export default function FormEditor() {
                                                                                   .target
                                                                                   .value,
                                                                             }
-                                                                          : f,
+                                                                          : o,
                                                                     ),
                                                                 }
                                                               : f,
